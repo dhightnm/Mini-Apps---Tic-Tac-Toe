e62bad8eff063b2f4d25c627a57d6dd0438d68af
dhightnm <dhightnm@gmail.com>
2021-05-24 19:14:19 -0700

    Implemented Reset

--- Tic-Tac-Toe/app.js
+++ Tic-Tac-Toe/app.js
@@ -7,7 +7,7 @@ var boardGrid = [
 ];
 
 var move = 'X';
-var gameState = true;
+var State = true;
 
 // let gameStart = () => {
 //   document.move = 'X';
@@ -15,13 +15,12 @@ var gameState = true;
 
 var playerMove = (element) => {
   // debugger;
-  if (element.innerHTML.length === 0 && gameState) {
+  if (element.innerHTML.length === 0 && State) {
     var row = element.parentNode.id[element.parentNode.id.length - 1] - 1;
     var column = element.id[element.id.length - 1];
     element.innerHTML = move;
     boardGrid[row][column] = move;
-    checkWinRow();
-    checkWinColumn();
+    winCheckerAll()
     move = move === 'X' ? 'O' : 'X';
   }
 };
@@ -34,6 +33,39 @@ let turnToggle = () => {
   }
 
 };
+
+let reset = () => {
+  let domCells = document.getElementsByClassName('column');
+  var boardGrid = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+
+
+
+  // boardGrid.forEach(element => {
+  //   console.log(element);
+  //     element.map(cell => {
+  //       console.log(cell);
+  //       cell = '';
+  //       console.log(cell);
+  //       console.log(boardGrid);
+
+  //     });
+  //   });
+  for (let i = 0; i < domCells.length; i++) {
+    domCells[i].innerHTML = '';
+  }
+
+  State = true;
+
+};
+
+let winMessage = () => {
+  alert('You have won the game!');
+  State = false;
+}
 
 //iterate through board and check if cells have X or O
 let checkWinRow = (row, column) => {
@@ -48,8 +80,9 @@ let checkWinRow = (row, column) => {
           win = false;
         }
       }
-      if (win)
-       alert('You have won the game');
+      if (win) {
+    winMessage()
+      }
     }
   }
 };
@@ -65,10 +98,43 @@ let checkWinColumn = (row, column) => {
         }
       }
       if (win) {
-        alert('You have won the game');
+        winMessage();
       }
     }
   }
 };
-  //if
 
+let checkWinDiagonalMajor = () => {
+  let move = boardGrid[0][0];
+  let win = true;
+  if (move !== '') {
+    boardGrid.forEach((element, idx) => {
+      if (element[idx] !== move){
+        win = false;
+
+      }
+    });
+    if (win) {
+      winMessage();
+    }
+  }
+};
+
+let checkWinDiagonalMinor = (row, column) => {
+  let move = boardGrid[boardGrid.length - 1][0];
+  let win = true;
+  if (move !== '') {
+    for (let row = boardGrid.length - 2, column = 1; row >= 0; row--, column++) {
+      if (boardGrid[row][column] !== move)
+      win = false;
+    }
+    if (win) {
+      winMessage();
+    }
+  }
+};
+
+let winCheckerAll = () => {
+  return checkWinRow() || checkWinColumn() || checkWinDiagonalMajor() || checkWinDiagonalMinor();
+
+};
